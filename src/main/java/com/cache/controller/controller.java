11aaf09398c6bb8993813103b74bfdd7a7f31c4c
Nooruddin <shaiknooruddin981@gmail.com>
2022-11-03 23:26:26 +0530

    Added Discover services and Hazelcash also added

--- src/main/java/com/cache/controller/controller.java
+++ src/main/java/com/cache/controller/controller.java
@@ -5,11 +5,15 @@ import java.util.Map;
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.cache.services.ServiceProvider;
 
@@ -18,6 +22,12 @@ import com.cache.services.ServiceProvider;
 public class controller {
 	@Autowired
 	private ServiceProvider serviceProvider;
+	
+	@Autowired
+	private RestTemplate template;
+	
+	@Value("${Descovery_clint}")
+	private String uri;
 	
 	@Cacheable(cacheNames = "Device",key = "#Device_Type")
 	@GetMapping("/set")
@@ -31,5 +41,11 @@ public class controller {
 	public Map<String, Double> Device_Name(@PathVariable("Device_Name") String Device_Name ){
 		System.out.println("this is method working");
 		return serviceProvider.GetPriceOfDevice(Device_Name);
+	}
+	
+	@GetMapping("/call")
+	public ResponseEntity<String> get(){
+		String object = template.getForObject(uri, String.class);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(object);
 	}
 }
